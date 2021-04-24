8e2e0712499e1f886af27cef31524293d6be45d8
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2021-04-24 15:17:24 -0400

    Finished reorganizing project folders

---
+++ Ragdoll Race/Assets/Players/Scripts/Active Ragdoll Control/ArmsIKCalculator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DitzelGames.FastIK;
+
+public class ArmsIKCalculator : MonoBehaviour
+{
+
+    [Header("References")]
+    [SerializeField] private ActiveRagdoll activeRagdoll;
+    [SerializeField] private Rigidbody pelvisRigidbody;
+    [Space]
+    [SerializeField] private FastIKFabric leftArmIK;
+    [SerializeField] private FastIKFabric rightArmIK;
+    [Space]
+    [SerializeField] private Transform leftArmRoot;
+    [SerializeField] private Transform rightArmRoot;
+
+    [Header("Punching Properties")]
+    [SerializeField] private float punchRecoveryDuration;
+
+    [Header("Climbing Properties")]
+    [SerializeField] private LayerMask climbableLayers;
+
+    
+
+    // Unity Functions
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(!activeRagdoll.player.isRagdoll){
+
+        }
+    }
+}
