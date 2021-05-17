1b677103994d798a005166ef20b89e30aacad309
Grant Keele <31322729+grantkeele@users.noreply.github.com>
2021-05-16 20:27:28 -0400

    Added moving platform component to tank treads

--- Ragdoll Race/Assets/Stages/Tank Canyon/Tank/Scripts/ConveyorBelt.cs
+++ Ragdoll Race/Assets/Stages/Tank Canyon/Tank/Scripts/ConveyorBelt.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour
 {
     private Rigidbody beltRigidbody;
+    private MovingPlatform movingPlatform;
 
     [HideInInspector] public float speed;
 
@@ -12,15 +13,20 @@ public class ConveyorBelt : MonoBehaviour
     private void Awake()
     {
         beltRigidbody = GetComponent<Rigidbody>();
+        movingPlatform = GetComponent<MovingPlatform>();
     }
 
 
     void FixedUpdate()
     {
+        // Shift the "conveyor belt" forward, then teleport it back
         Vector3 displacement = transform.forward * speed * Time.fixedDeltaTime;
 
         beltRigidbody.position -= displacement;
         beltRigidbody.MovePosition(beltRigidbody.position + displacement);
+
+        // Set the moving platform component's velocity
+        movingPlatform.velocity = transform.forward * speed;
     }
 
 }
